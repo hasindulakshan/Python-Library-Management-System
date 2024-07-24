169317c2e7a82b8351d275b868c18812db7a6a20
Hasindu Lakshan <hasindulakshan0040@gmail.com.com>
2024-07-24 18:17:42 +0530

    upload models

---
+++ cdsmodel.py
@@ -0,0 +1,7 @@
+class Compactdisk:
+    def __init__(self, cd_no, title, subject, rental_price, num_copies):
+        self.cd_no = cd_no  
+        self.title = title 
+        self.subject = subject
+        self.rental_price = rental_price
+        self.num_copies = num_copies
